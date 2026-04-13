52fcbfd1cd3a4d76f55c254cded2553c26804261
angguncahaya27 <12450121421@students.uin-suska.ac.id>
2026-04-13 12:37:34 +0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+ import { useEffect, useState } from "react";
 import { Routes, Route, useNavigate, useParams } from "react-router-dom";
 import "./App.css";
 
